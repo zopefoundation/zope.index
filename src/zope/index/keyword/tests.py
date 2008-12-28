1c0f4da98378f5c9bfd33da58f0f63317f300b34
Dan Korostelev <nadako@gmail.com>
2008-12-28 06:40:44 +0000

    Optimize KeywordIndex searching. Make KeywordIndex implement IIndexSearch. Use IFSet rather than IISet to make it possible to merge KeywordIndex results with other indexes.

--- src/zope/index/keyword/tests.py
+++ src/zope/index/keyword/tests.py
@@ -23,7 +23,7 @@ from zope.interface.verify import verifyClass
 
 class KeywordIndexTest(TestCase):
 
-    from BTrees.IIBTree import IISet
+    from BTrees.IFBTree import IFSet
 
     def setUp(self):
         self.index = KeywordIndex()
@@ -41,7 +41,7 @@ class KeywordIndexTest(TestCase):
 
         results = self.index.search(query, mode)
 
-        # results and expected are IISets() but we can not
+        # results and expected are IFSets() but we can not
         # compare them directly since __eq__() does not seem
         # to be implemented for BTrees
 
@@ -52,6 +52,18 @@ class KeywordIndexTest(TestCase):
 
     def _search_or(self, query, expected):
         return self._search(query, expected, 'or')
+
+    def _apply(self, query, expected, mode='and'):
+        results = self.index.apply(query)
+        self.assertEqual(results.keys(), expected.keys())
+
+    def _apply_and(self, query, expected):
+        results = self.index.apply({'operator': 'and', 'query': query})
+        self.assertEqual(results.keys(), expected.keys())
+
+    def _apply_or(self, query, expected):
+        results = self.index.apply({'operator': 'or', 'query': query})
+        self.assertEqual(results.keys(), expected.keys())
 
     def test_interface(self):
         verifyClass(IInjection, KeywordIndex)
@@ -85,13 +97,13 @@ class KeywordIndexTest(TestCase):
         self.index.index_doc(1,  ('foo', 'bar', 'doom'))
         self.index.index_doc(1,  ('bar', 'blabla'))
         self.assertEqual(self.index.documentCount(), 3)
-        self._search('quick',   self.IISet())
-        self._search('foo',   self.IISet())
-        self._search('bar',   self.IISet([1]))
-        self._search(['doom'],   self.IISet())
-        self._search(['blabla'],   self.IISet([1]))
-        self._search_and(('bar', 'blabla'),   self.IISet([1]))
-        self._search(['cmf'],   self.IISet([5]))
+        self._search('quick',   self.IFSet())
+        self._search('foo',   self.IFSet())
+        self._search('bar',   self.IFSet([1]))
+        self._search(['doom'],   self.IFSet())
+        self._search(['blabla'],   self.IFSet([1]))
+        self._search_and(('bar', 'blabla'),   self.IFSet([1]))
+        self._search(['cmf'],   self.IFSet([5]))
 
     def test_hasdoc(self):
         self._populate_index()
@@ -104,25 +116,46 @@ class KeywordIndexTest(TestCase):
 
     def test_simplesearch(self):
         self._populate_index()
-        self._search([''],      self.IISet())
-        self._search(['cmf'],   self.IISet([1, 5]))
-        self._search(['zope'],  self.IISet([1, 3]))
-        self._search(['zope3'], self.IISet([1]))
-        self._search(['foo'],   self.IISet())
+        self._search([''],      self.IFSet())
+        self._search(['cmf'],   self.IFSet([1, 5]))
+        self._search(['zope'],  self.IFSet([1, 3]))
+        self._search(['zope3'], self.IFSet([1]))
+        self._search(['foo'],   self.IFSet())
 
     def test_search_and(self):
         self._populate_index()
-        self._search_and(('cmf', 'zope3'), self.IISet([1]))
-        self._search_and(('cmf', 'zope'),  self.IISet([1]))
-        self._search_and(('cmf', 'zope4'), self.IISet())
-        self._search_and(('zope', 'ZOPE'), self.IISet([1, 3]))
+        self._search_and(('cmf', 'zope3'), self.IFSet([1]))
+        self._search_and(('cmf', 'zope'),  self.IFSet([1]))
+        self._search_and(('cmf', 'zope4'), self.IFSet())
+        self._search_and(('zope', 'ZOPE'), self.IFSet([1, 3]))
 
     def test_search_or(self):
         self._populate_index()
-        self._search_or(('cmf', 'zope3'), self.IISet([1, 5]))
-        self._search_or(('cmf', 'zope'),  self.IISet([1, 3, 5]))
-        self._search_or(('cmf', 'zope4'), self.IISet([1, 5]))
-        self._search_or(('zope', 'ZOPE'), self.IISet([1,3]))
+        self._search_or(('cmf', 'zope3'), self.IFSet([1, 5]))
+        self._search_or(('cmf', 'zope'),  self.IFSet([1, 3, 5]))
+        self._search_or(('cmf', 'zope4'), self.IFSet([1, 5]))
+        self._search_or(('zope', 'ZOPE'), self.IFSet([1,3]))
+
+    def test_apply(self):
+        self._populate_index()
+        self._apply(('cmf', 'zope3'), self.IFSet([1]))
+        self._apply(('cmf', 'zope'),  self.IFSet([1]))
+        self._apply(('cmf', 'zope4'), self.IFSet())
+        self._apply(('zope', 'ZOPE'), self.IFSet([1, 3]))
+
+    def test_apply_and(self):
+        self._populate_index()
+        self._apply_and(('cmf', 'zope3'), self.IFSet([1]))
+        self._apply_and(('cmf', 'zope'),  self.IFSet([1]))
+        self._apply_and(('cmf', 'zope4'), self.IFSet())
+        self._apply_and(('zope', 'ZOPE'), self.IFSet([1, 3]))
+
+    def test_apply_or(self):
+        self._populate_index()
+        self._apply_or(('cmf', 'zope3'), self.IFSet([1, 5]))
+        self._apply_or(('cmf', 'zope'),  self.IFSet([1, 3, 5]))
+        self._apply_or(('cmf', 'zope4'), self.IFSet([1, 5]))
+        self._apply_or(('zope', 'ZOPE'), self.IFSet([1,3]))
 
     def test_index_input(self):
         self.assertRaises(
@@ -131,7 +164,7 @@ class KeywordIndexTest(TestCase):
 
 class KeywordIndexTest64(KeywordIndexTest):
 
-    from BTrees.LLBTree import LLSet as IISet
+    from BTrees.LFBTree import LFSet as IFSet
 
     def setUp(self):
         self.index = KeywordIndex(family=BTrees.family64)
