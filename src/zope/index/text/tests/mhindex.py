cc24f46e1b2c6d72bb1cff7bf2430c0532b14260
Gediminas Paulauskas <menesis@pov.lt>
2012-11-28 18:11:15 +0000

    Cleanup

--- src/zope/index/text/tests/mhindex.py
+++ src/zope/index/text/tests/mhindex.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.4
+#!/usr/bin/env python
 ##############################################################################
 #
 # Copyright (c) 2003 Zope Foundation and Contributors.
