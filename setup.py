e8ca29303b2fbff4abb9f936263faed9eba3f238
Michael Howitz <mh@gocept.com>
2022-04-06 08:37:09 +0200

    Back to development: 5.2.1

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ class optional_build_ext(build_ext):
 
 
 setup(name='zope.index',
-      version='5.2.0',
+      version='5.2.1.dev0',
       url='https://github.com/zopefoundation/zope.index',
       license='ZPL 2.1',
       author='Zope Foundation and Contributors',
